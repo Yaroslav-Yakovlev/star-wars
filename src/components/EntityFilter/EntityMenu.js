28ed0d7d54f65f1236fc61b5a2b739570eb48faa
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-06-18 14:32:21 +0300

    refactored EntityFilter component

---
+++ src/components/EntityFilter/EntityMenu.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Menu, MenuItem } from '@mui/material';
+import { capitalize } from '../../utils';
+import { useSelector } from 'react-redux';
+import { selectListOfEntities } from '../../store/selectors';
+import { useTheme } from '@mui/material/styles';
+
+const EntityMenu = ({ handleCloseMenu, anchorEl, isOpenMenu, handleSelectItem }) => {
+  const listOfEntities = useSelector(selectListOfEntities);
+
+  const theme = useTheme();
+
+  return (
+    <Menu
+      id="basic-menu"
+      anchorEl={anchorEl}
+      open={isOpenMenu}
+      onClose={handleCloseMenu}
+      MenuListProps={{
+        'aria-labelledby': 'basic-button',
+      }}
+      PaperProps={{
+        sx: {
+          backgroundColor: theme.palette.primary.light,
+        },
+      }}
+    >
+      {listOfEntities.map(entity => (
+        <MenuItem
+          key={entity}
+          value={entity}
+          onClick={handleSelectItem}
+          sx={{ color: theme.palette.text.main }}
+        >
+          {capitalize(entity)}
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+};
+
+export default EntityMenu;
