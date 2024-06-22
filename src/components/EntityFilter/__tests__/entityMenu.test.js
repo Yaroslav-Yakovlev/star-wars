5877bd45cd07238d120050b64b795ab2d1ae1776
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-06-22 15:24:01 +0300

    added test for EntityMenu component

--- src/components/EntityFilter/__tests__/entityMenu.test.js
+++ src/components/EntityFilter/__tests__/entityMenu.test.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { screen, render } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
 import EntityMenu from '../EntityMenu';
+import { capitalize } from '../../../utils/index';
 
 const mockStore = configureMockStore();
 
@@ -15,10 +16,15 @@ const rendersWithMockStore = (store, props) => {
 };
 
 describe('EntityMenu component', () => {
-  const store = mockStore({
-    entities: {
-      listOfEntities: ['all', 'people', 'planets', 'starships', 'vehicles'],
-    },
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      entities: {
+        listOfEntities: ['all', 'people', 'planets', 'starships', 'vehicles'],
+      },
+    });
+
   });
 
   it('should render EntityMenu component', () => {
@@ -28,8 +34,33 @@ describe('EntityMenu component', () => {
       isOpenMenu: true,
       handleSelectItem: jest.fn(),
     };
+
     rendersWithMockStore(store, props);
 
     expect(screen.getByRole('entity-menu')).toBeInTheDocument();
+  });
+
+  it('should render EntityMenu with list of entities', () => {
+    const props = {
+      handleCloseMenu: jest.fn(),
+      anchorEl: document.createElement('div'),
+      isOpenMenu: true,
+      handleSelectItem: jest.fn(),
+    };
+
+    rendersWithMockStore(store, props);
+
+    const mockListOfEntities = [
+      'all',
+      'people',
+      'planets',
+      'starships',
+      'vehicles'];
+
+    mockListOfEntities.map(entity => {
+      const menuItem = screen.getByRole(entity);
+      const textContent = capitalize(entity);
+      expect(menuItem).toHaveTextContent(textContent);
+    });
   });
 });
